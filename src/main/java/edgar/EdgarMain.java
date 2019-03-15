fd34071ad6f12cdc4c02b5135cf79965ea95e78f
Philip Wiegratz <wiegratz.p@gmail.com>
2019-03-15 18:33:22 +0100

    set parser to parse all files in folder

--- src/main/java/edgar/EdgarMain.java
+++ src/main/java/edgar/EdgarMain.java
@@ -1,59 +1,63 @@
 package edgar;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.URL;
 import java.util.HashMap;
 
 public class EdgarMain {
 
-	static String tsvPath = "C:\\Users\\phili\\Desktop\\2010-QTR1.tsv";
-	static HashMap<String, String> lol = new HashMap<String, String>();
-	static String baseUrl = "https://www.sec.gov/Archives/";
-	
-	public static void main(String[] args) {
-		
-		System.out.println(tsvPath);
-		try {
-			parseFile();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+    static String folderPath = "C:\\Users\\phili\\Desktop\\tsv";
+    static File folder = new File(folderPath);
+    static File[] listOfFiles = folder.listFiles();
+    static HashMap<String, String> lol = new HashMap<String, String>();
+    static String baseUrl = "https://www.sec.gov/Archives/";
 
-	}
-	
-	public static void parseFile() throws IOException {
-		        BufferedReader br = new BufferedReader(new FileReader(tsvPath));
-		        String row = "";
-		        String currentCompany = "";
-		        String currentSIC = "";
-		        while ((row = br.readLine()) != null){
-		        		currentCompany = row.split("\\|")[1];
-		        		if (!lol.containsKey(currentCompany)) {
-		        		currentSIC = getSIC(row.split("\\|")[4]);
-		        			if (currentSIC != null) {
-				        		lol.put(currentCompany, currentSIC);
-				        		System.out.println(currentCompany + " = " +lol.get(currentCompany));
-				        		currentSIC = null;
-		        			}
-		        		}
-		        }
-		        br.close();
-	}
+    public static void main(String[] args) {
 
-	private static String getSIC(String currentCompany) throws IOException {
-			URL url = new URL(baseUrl+currentCompany);
-	        BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
-	        
-	        String inputLine;
-	        while ((inputLine = br.readLine()) != null)
-	            if (inputLine.split("STANDARD INDUSTRIAL CLASSIFICATION:").length >= 2) {
-	            	return inputLine.split("STANDARD INDUSTRIAL CLASSIFICATION:")[1].split("\\[")[1].substring(0, 4);
-	            }
-	        br.close();
-	        return null;
-	}
+        parseFolder();
+
+    }
+
+    public static void parseFolder() {
+        for (File i : listOfFiles) {
+            try {
+                parseFile(i.getPath());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void parseFile(String tsvPath) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(tsvPath));
+        String row = "";
+        String currentCompany = "";
+        String currentSIC = "";
+        while ((row = br.readLine()) != null) {
+            currentCompany = row.split("\\|")[1];
+            if (!lol.containsKey(currentCompany)) {
+                currentSIC = getSIC(row.split("\\|")[4]);
+                if (currentSIC != null) {
+                    lol.put(currentCompany, currentSIC);
+                    System.out.println(currentCompany + " = " + lol.get(currentCompany));
+                    currentSIC = null;
+                }
+            }
+        }
+        br.close();
+    }
+
+    private static String getSIC(String currentCompany) throws IOException {
+        URL url = new URL(baseUrl + currentCompany);
+        BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+
+        String inputLine;
+        while ((inputLine = br.readLine()) != null)
+            if (inputLine.split("STANDARD INDUSTRIAL CLASSIFICATION:").length >= 2) {
+                return inputLine.split("STANDARD INDUSTRIAL CLASSIFICATION:")[1].split("\\[")[1].substring(0, 4);
+            }
+        br.close();
+        return null;
+    }
 
 }
